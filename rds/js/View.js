6674a92d05c101fe5059550d0fbc26f000b2ecc6
Peter Heiss <peter.heiss@uni-muenster.de>
2020-10-20 15:53:05 +0200

    now fixes filepatch

--- rds/js/View.js
+++ rds/js/View.js
@@ -211,7 +211,7 @@
               this[index].filepath = targetPath.trim();
             }
           }, self._services.getAll());
-          $("#filepath").html('<span id="fileStorage-path-Owncloud">' + t("rds", "Current path:") + " " + targetPath.trim() + '</span>')
+          $("#filepath").html(t("rds", "Current path:") + ' ' + '<span id="fileStorage-path-Owncloud">' + targetPath.trim() + '</span>')
         },
         false,
         "httpd/unix-directory",
